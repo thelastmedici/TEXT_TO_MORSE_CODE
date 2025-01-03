ff2a7db2fc9a2f0f70aa3b697492845303b21113
thelastmedici <christianopeyemijoshua@gmail.com>
2025-01-03 10:07:54 +0100

    added app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+"""
+     This program provides functionality to convert text to morse code and vice versa.
+ """
+print("Welcome to my Morse code converter app!\n")
+
+# create a dictionary for morse code
+letters = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+            'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+            'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+            'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+            'Y': '-.--', 'Z': '--..'}
+numbers = {'1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+            '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----'}
+
+symbols = {'.': '.-.-.-', ',': '--..--', '?': '..--..', '!': '-.-.--',
+            '/': '-..-.', '()': '-.--.-',
+            '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-',
+            '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.',
+            '$': '...-..-', '@': '.--.-.'}
+
+MORSE_CODE_DICT = {**letters, **numbers, **symbols}
+# ask for what user wants to do
+user_choice = input("TYPE 'E' for encode or 'D' for decode: ").upper()
+if user_choice == "E":
+   user_text = input("Enter the text you want to convert to morse code: ").upper()
+# function to encode
+# def encode_to_morse():
+#    user_text = input("Enter the text you want to convert to morse: ").upper()
+#    morse_code = ""
+
+ # function to decode
+ # def decode_from_morse():
